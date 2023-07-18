d430aa2db505fafb487a493b5a70c0a002247ffa
zikoom <hunmonhun@gmail.com>
2023-07-18 12:29:30 +0900

    Postteet

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 import { ApolloServer, gql } from "apollo-server";
 
+
+const Tweets = [
+  {
+    id: '1',
+    text: 'hahaha'
+  },
+  {
+    id: '2',
+    text: 'hohoho'
+  }
+]
+
 const typeDefs = gql`
 
   type User {
@@ -8,14 +20,14 @@ const typeDefs = gql`
   }
 
   type Tweet {
-    id: ID
-    text: String
+    id: ID!
+    text: String!
     author: User
   }
 
   type Query {
     allTweets: [Tweet]
-    tweet(id: ID!): Tweet!
+    tweet(id: ID!): Tweet
   }
 
   type Mutation {
@@ -23,7 +35,30 @@ const typeDefs = gql`
   }
 `;
 
-const server = new ApolloServer({typeDefs})
+const resolvers = {
+  Query: {
+    tweet() {
+      console.log('im called');
+      return null;
+    },
+
+    allTweets() {
+      return Tweets;
+    }
+  },
+  Mutation: {
+    postTweet(root, params){
+      console.log(root, params);
+      return {
+        id: 1,
+        text: 'haha',
+
+      }
+    }
+  }
+}
+
+const server = new ApolloServer({typeDefs, resolvers})
 
 server.listen().then(({url}) => {
   console.log(`running on ${url}`)
