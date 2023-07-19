2a119a218120f9ffa3fd4f91c756152bb3f71ccd
zikoom <hunmonhun@gmail.com>
2023-07-19 13:42:02 +0900

    node-fetch movie schema

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import { ApolloServer, gql } from "apollo-server";
-
+import fetch from "node-fetch";
 
 const Tweets = [
   {
@@ -44,13 +44,38 @@ const typeDefs = gql`
   }
 
   type Query {
+    allMovies: [Movie!]!
     allUsers: [User!]!
     allTweets: [Tweet]
     tweet(id: ID!): Tweet
+    movie(id: String!): Movie
   }
 
   type Mutation {
     postTweet(text: String, userId: ID): Tweet
+  }
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String
+    description_full: String!
+    synopsis: String
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
   }
 `;
 
@@ -68,6 +93,18 @@ const resolvers = {
     allUsers() {
       return users;
     },
+
+    async allMovies() {
+      const r = await fetch("https://yts.mx/api/v2/list_movies.json");
+      const json = await r.json();
+      return json.data.movies;
+    },
+
+    async movie(root, {id}) {
+      const r = await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`);
+      const json = await r.json();
+      return json.data.movie;
+    }
 
 
   },
